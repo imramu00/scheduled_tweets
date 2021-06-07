64fb59a1015fe11a4e13991160c883036483feda
Ramanathan <imramu00@gmail.com>
2021-06-07 16:54:16 +0530

    Added Sessions

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,1 @@
+class PasswordsController < ActionController::Base
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
   root to: "main#index"
 
   get 'about-us', to: "about#index", as: :about
-  get 'sign_up', to: "registrations#new"
+
+  get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  get "password", to: "passwords#edit"
+  patch "password", to: "passwords#update"
+
   delete "logout", to: "sessions#destroy"
 end
